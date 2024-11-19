f6b2b4b5df4fed39d28b75dab0cf78934d2dba5c
vitalii615 <bahmachronal@gmail.com>
2024-11-19 20:26:14 +0200

    add Widget TextField in login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import '../widgets/custom_textfield.dart'; 
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return null
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Логін'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0), // Відступи навколо
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Центрування елементів
+          children: [
+            const Text(
+              'Ліга Чемпіонів 2024',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            const CustomTextField(hintText: 'Електронна пошта'), // Використання кастомного поля
+            const SizedBox(height: 10),
+            const CustomTextField(hintText: 'Пароль', isPassword: true), // Поле пароля
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
   }
 }
