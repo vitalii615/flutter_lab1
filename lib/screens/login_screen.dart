ca5d965288da1f1855a3d4ed13a29c1d3e9d0f5b
vitalii615 <bahmachronal@gmail.com>
2024-11-22 16:18:45 +0200

    add check on valid data in login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,9 +3,33 @@ import '../widgets/custom_textfield.dart';
 import '../widgets/custom_button.dart';   
 import 'registration_screen.dart'; 
 import 'home_screen.dart'; 
+import '../storage/user_storage.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late UserStorage _userStorage;
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _userStorage = SharedPreferencesUserStorage();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,46 +42,88 @@ class LoginScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(screenWidth * 0.04), // Відступи
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [
-            Text(
-              'Ліга Чемпіонів 2024',
-              style: TextStyle(
-                fontSize: screenWidth * 0.06, 
-                fontWeight: FontWeight.bold,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Ліга Чемпіонів 2024',
+                style: TextStyle(
+                  fontSize: screenWidth * 0.06,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: screenHeight * 0.03), // Висота відступу
-            const CustomTextField(hintText: 'Електронна пошта'),
-            SizedBox(height: screenHeight * 0.015), 
-            const CustomTextField(hintText: 'Пароль', isPassword: true),
-            SizedBox(height: screenHeight * 0.03), // Відступ перед кнопкою
-            CustomButton(
-              text: 'Увійти',
-              onPressed: () {
-                Navigator.push<HomeScreen>(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()),
-                );
-              },
-            ),
-            SizedBox(height: screenHeight * 0.02), // Відступ між кнопками
-            TextButton(
-              onPressed: () {
-                Navigator.push<RegistrationScreen>(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegistrationScreen()),
-                );
-              },
-              child: Text(
-                'Зареєструватися',
-                style: TextStyle(fontSize: screenWidth * 0.045),
+              SizedBox(height: screenHeight * 0.03), // Висота відступу
+              CustomTextField(
+                hintText: 'Електронна пошта',
+                controller: _emailController,
               ),
-            ),
-          ],
+              SizedBox(height: screenHeight * 0.015), 
+              CustomTextField(
+                hintText: 'Пароль',
+                isPassword: true,
+                controller: _passwordController,
+              ),
+              SizedBox(height: screenHeight * 0.03), // Відступ перед кнопкою
+              CustomButton(
+                text: 'Увійти',
+                onPressed: () async {
+                  final userData = await _userStorage.getUserData();
+                  if (_emailController.text == userData['email'] && _passwordController.text == userData['password']) {
+                    Navigator.push<void>(
+                      context,
+                      MaterialPageRoute<void>(builder: (context) => const HomeScreen()),
+                    );
+                  } else {
+                    _showLoginErrorDialog();
+                  }
+                },
+              ),
+              SizedBox(height: screenHeight * 0.02), // Відступ між кнопками
+              TextButton(
+                onPressed: () {
+                  Navigator.push<void>(
+                    context,
+                    MaterialPageRoute<void>(builder: (context) => const RegistrationScreen()),
+                  );
+                },
+                child: Text(
+                  'Зареєструватися',
+                  style: TextStyle(fontSize: screenWidth * 0.045),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  final _formKey = GlobalKey<FormState>();
+
+  void _showLoginErrorDialog() {
+    showDialog<void>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text(
+            'Помилка входу',
+            style: TextStyle(color: Colors.red),
+          ),
+          content: const Text(
+            'Невірна електронна пошта або пароль. Будь ласка, спробуйте ще раз.',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Зрозуміло'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
